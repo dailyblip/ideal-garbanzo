124d2b6247ead54acb5291edfeeeb8d71cc2a5d4
dailyblip <dannyrykers@gmail.com>
2026-09-04 04:47:28 -0700

    Protect T5 employer-direct links

--- scripts/validate-priority-employer-sources.mjs
+++ scripts/validate-priority-employer-sources.mjs
@@ -17,6 +17,7 @@ const officialHostsByCompany = new Map([
   ['Iron Mountain', new Set(['ironmountain.wd5.myworkdayjobs.com'])],
   ['Compass Datacenters', new Set(['compass-datacenters.breezy.hr'])],
   ['Flexential', new Set(['job-boards.greenhouse.io'])],
+  ['T5 Data Centers', new Set(['jobs.lever.co'])],
   ['Vantage Data Centers', new Set(['vantagedc.wd1.myworkdayjobs.com'])],
   ['QTS Data Centers', new Set(['qtsdatacenters.wd5.myworkdayjobs.com'])],
   ['CyrusOne', new Set(['cyrusone.wd1.myworkdayjobs.com'])],
